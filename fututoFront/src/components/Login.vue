8b6355b2dcafef359cb85f845b8c840664b17ee6
uboat46 <uboat46@hotmail.com>
2017-10-08 14:29:44 -0500

    added collage and forms

--- fututoFront/src/components/Login.vue
+++ fututoFront/src/components/Login.vue
@@ -3,9 +3,9 @@
     <!-- Navbar (sit on top) -->
 <div class="w3-top">
   <div class="w3-bar w3-white w3-card-2" id="myNavbar">
-    <a href="#home" class="w3-bar-item w3-button w3-wide">FUTURO MÉXICO</a>
+    <a class="w3-bar-item w3-button w3-wide">FUTURO MÉXICO</a>
     <!-- Right-sided navbar links -->
-    <form @submit.prevent="login">
+    <form @submit.prevent="login" class="w3-hide-small">
     <div class="w3-right w3-margin-right w3-hide-small">
       <a 
       class="w3-bar-item w3-button"
@@ -32,7 +32,7 @@
 
     <!-- Hide right-floated links on small screens and replace them with a menu icon -->
 
-    <a href="javascript:void(0)" class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium" onclick="w3_open()">
+    <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hide-medium w3-text-black w3-padding-0" @click.prevent="w3_open">
       <i class="fa fa-bars"></i>
     </a>
   </div>
@@ -40,30 +40,49 @@
 
 <!-- Sidebar on small screens when clicking the menu icon -->
 <nav class="w3-sidebar w3-bar-block w3-black w3-card-2 w3-animate-left w3-hide-medium w3-hide-large" style="display:none" id="mySidebar">
-  <a href="javascript:void(0)" onclick="w3_close()" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
-  <a href="#about" onclick="w3_close()" class="w3-bar-item w3-button">ABOUT</a>
-  <a href="#team" onclick="w3_close()" class="w3-bar-item w3-button">TEAM</a>
-  <a href="#work" onclick="w3_close()" class="w3-bar-item w3-button">WORK</a>
-  <a href="#pricing" onclick="w3_close()" class="w3-bar-item w3-button">PRICING</a>
-  <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button">CONTACT</a>
+  <a @click.prevent="w3_close" class="w3-bar-item w3-button w3-large w3-padding-16">Close ×</a>
+  <form @submit.prevent="login">
+    <div>
+      <a 
+      class="w3-bar-item w3-button"
+      @click.prevent>
+        <input type="email" name="email" placeholder="Correo electrónico">
+      </a>
+      <a 
+      class="w3-bar-item w3-button"
+      @click.prevent>
+        <input type="password" name="password" placeholder="Contraseña">
+      </a>
+      <a 
+      class="w3-bar-item w3-button"
+      @click.prevent="w3_close">
+        <input class="w3-round-large w3-text-white" type="submit" value="Inicia Sesión">
+      </a>
+      <a
+      class="w3-bar-item w3-button"
+      @click.prevent="w3_close">
+        ¿Has olvidado tu contraseña?
+      </a>
+    </div>
+    </form>
 </nav>
 
 <!-- Header with full-height image -->
 <header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
-  <div class="w3-display-left w3-text-white" style="padding:48px">
-    <span class="w3-jumbo w3-hide-small">Start something that matters</span><br>
-    <span class="w3-xxlarge w3-hide-large w3-hide-medium">Start something that matters</span><br>
-    <span class="w3-large">Stop wasting valuable time with projects that just isn't you.</span>
-    <p><a href="#about" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Learn more and start today</a></p>
-  </div> 
-  <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <div class="w3-card-4 w3-display-left w3-margin-left w3-white">
+  <header class="w3-container w3-light-grey">
+    <h3>Destaca tu ayuda, es gratis!</h3>
+  </header>
+  <div class="w3-container">
+    <form> 
+      <input class="w3-input" type="text" name="nombre" placeholder="Nombre"> <br>
+      <input class="w3-input" type="text" name="apaellido" placeholder="Apellidos"> <br>
+      <input class="w3-input" type="email" name="email" placeholder="Correo eletrónico"> <br>
+      <input class="w3-input" type="password" name="password" placeholder="Contraseña"><br>
+      <input class="w3-button w3-block w3-dark-grey" type="submit" value="Registrarse">
+    </form>
   </div>
+</div>
 </header>
   </div>
 </template>
@@ -79,6 +98,18 @@ export default {
   methods : {
     login() {
       alert('login');
+    },
+    w3_open() {
+      var mySidebar = document.getElementById("mySidebar");
+      if (mySidebar.style.display === 'block') {
+          mySidebar.style.display = 'none';
+      } else {
+          mySidebar.style.display = 'block';
+      }
+    },
+    w3_close() {
+      var mySidebar = document.getElementById("mySidebar");
+      mySidebar.style.display = "none";
     }
   }
 }
@@ -94,7 +125,7 @@ export default {
 .bgimg-1 {
     background-position: center;
     background-size: cover;
-    background-image: url("/assets/collage.jpg");
+    background-image: url("../assets/collage.jpg");
     min-height: 100%;
 }
 </style>
